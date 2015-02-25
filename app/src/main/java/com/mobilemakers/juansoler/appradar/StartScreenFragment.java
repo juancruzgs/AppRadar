846683592000500ed7ea5b6e68fc5cf2bc8609ec
juancruzgs <juancruz_gs@hotmail.com>
2015-02-25 17:28:55 -0300

    Code Refactoring. Deleted unused imports.

--- app/src/main/java/com/mobilemakers/juansoler/appradar/StartScreenFragment.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/StartScreenFragment.java
@@ -1,20 +1,14 @@
 package com.mobilemakers.juansoler.appradar;
 
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 
 public class StartScreenFragment extends Fragment implements DestinationsDialog.DestinationDialogListener {

7996a83f7a546140b1bfdc5f8c61b47838e9f32a
unknown <paula.baudo@glb-d2968.globant.com>
2015-02-25 17:02:23 -0300

    Preference screen

--- app/src/main/java/com/mobilemakers/juansoler/appradar/MainActivity.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mobilemakers.juansoler.appradar;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -20,8 +21,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showIconInActionBar();
         initializeParse();
+        showIconInActionBar();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -58,6 +59,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent iSettings = new Intent(this, SettingsActivity.class);
+            startActivity(iSettings);
             return true;
         }
 
---
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/SettingsFragment.java
@@ -0,0 +1,19 @@
+package com.mobilemakers.juansoler.appradar;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    public SettingsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+}

bd71d53a0018cbd04d8a17d58186a8f8c83348b3
Paula Baudo <paula.baudo@globant.com>
2015-02-26 11:48:11 -0300

    MainActivity calls to StartScreenFragment

--- app/src/main/java/com/mobilemakers/juansoler/appradar/MainActivity.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/MainActivity.java
@@ -88,7 +88,7 @@ public class MainActivity extends ActionBarActivity implements ConnectionCallbac
     private void prepareFragment(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new SummaryFragment())
+                    .add(R.id.container, new StartScreenFragment())
                     .commit();
         }
     }

5c33fa243bd5667ea9a0788574733e266b974b8e
micaela.cavallo <micaela.cavallo@globant.com>
2015-03-05 14:13:07 -0300

    Update the screen information of summaryFragment

--- app/src/main/java/com/mobilemakers/juansoler/appradar/SummaryFragment.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/SummaryFragment.java
@@ -8,11 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 /**
@@ -20,14 +24,13 @@ import java.util.ArrayList;
  */
 public class SummaryFragment extends Fragment {
 
-    private final static int REFRESH_TIME = 10;
     private final static String NEXT_LOCATION = "nextLocation";
 
     TextView mTextViewDistance;
     TextView mTextViewRefreshTime;
     TextView mTextViewSpeedLimitValue;
 
-    int mDistance;
+    float mDistance;
     RadarList mRadars;
 
     public SummaryFragment() {
@@ -40,26 +43,14 @@ public class SummaryFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_summary, container, false);
         wireUpViews(rootView);
         getFragmentArguments();
-        mDistance = calculateDistanceToTheNextRadar(mRadars.get(0).getLatitude(), mRadars.get(0).getLongitude());
-        setDistance(mDistance);
-        mTextViewSpeedLimitValue.setText(String.format(getString(R.string.text_view_speed_limit_value_text), mRadars.get(0).getMaxSpeed()));
-        setRefreshTime(REFRESH_TIME);
+        setScreenInformation();
         return rootView;
     }
 
-    private int calculateDistanceToTheNextRadar(Double latitude, Double longitude) {
-        Location currentLocation = MainActivity.getLastLocation();
-        getFragmentArguments();
-        Location nextLocation = createTheNextLocation(latitude, longitude);
-        float distance = (currentLocation.distanceTo(nextLocation)/1000);
-        return Math.round(distance);
-    }
-
-    private Location createTheNextLocation(Double latitude, Double longitude) {
-        Location nextLocation = new Location(NEXT_LOCATION);
-        nextLocation.setLongitude(longitude);
-        nextLocation.setLatitude(latitude);
-        return nextLocation;
+    private void wireUpViews(View rootView) {
+        mTextViewDistance = (TextView) rootView.findViewById(R.id.text_view_distance);
+        mTextViewRefreshTime = (TextView) rootView.findViewById(R.id.text_view_refresh_time);
+        mTextViewSpeedLimitValue = (TextView) rootView.findViewById(R.id.text_view_speed_limit_value);
     }
 
     private void getFragmentArguments() {
@@ -69,19 +60,40 @@ public class SummaryFragment extends Fragment {
         }
     }
 
-    private void setRefreshTime(int refreshTime) {
-        mTextViewRefreshTime.setText(String.format(getString(R.string.text_view_refresh_time_text),Integer.toString(refreshTime)));
+    private void setScreenInformation() {
+        mDistance = calculateDistanceToTheNextRadar(mRadars.get(0).getLatitude(), mRadars.get(0).getLongitude());
+        setDistance(mDistance);
+        mTextViewSpeedLimitValue.setText(String.format(getString(R.string.text_view_speed_limit_value_text), mRadars.get(0).getMaxSpeed()));
+        setRefreshTime(getCurrentTime());
     }
 
-    private void setDistance(int distance) {
-        mTextViewDistance.setText(String.format(getString(R.string.text_view_distance_value), Integer.toString(distance)));
+    private float calculateDistanceToTheNextRadar(Double latitude, Double longitude) {
+        Location currentLocation = MainActivity.getLastLocation();
+        Location nextLocation = createTheNextLocation(latitude, longitude);
+        float distance = (currentLocation.distanceTo(nextLocation)/1000);
+        return new BigDecimal(distance).setScale(1,BigDecimal.ROUND_HALF_UP).floatValue();
     }
 
-    private void wireUpViews(View rootView) {
-        mTextViewDistance = (TextView) rootView.findViewById(R.id.text_view_distance);
-        mTextViewRefreshTime = (TextView) rootView.findViewById(R.id.text_view_refresh_time);
-        mTextViewSpeedLimitValue = (TextView) rootView.findViewById(R.id.text_view_speed_limit_value);
+    private Location createTheNextLocation(Double latitude, Double longitude) {
+        Location nextLocation = new Location(NEXT_LOCATION);
+        nextLocation.setLongitude(longitude);
+        nextLocation.setLatitude(latitude);
+        return nextLocation;
     }
+    private void setDistance(float distance) {
+        mTextViewDistance.setText(String.format(getString(R.string.text_view_distance_value), Float.toString(distance)));
+    }
+
+    private void setRefreshTime(String refreshTime) {
+        mTextViewRefreshTime.setText(String.format(getString(R.string.text_view_refresh_time_text), refreshTime));
+    }
+
+    private String getCurrentTime () {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        return simpleDateFormat.format(date);
+    }
+
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -89,10 +101,30 @@ public class SummaryFragment extends Fragment {
         inflater.inflate(R.menu.menu_fragment_summary, menu);
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Boolean handled = false;
+
+        switch(id) {
+            case R.id.action_bar:
+                handled = true;
+                break;
+            case R.id.action_refresh:
+                handled = true;
+                setScreenInformation();
+                break;
+        }
+
+        if (!handled) {
+            handled = super.onOptionsItemSelected(item);
+        }
+        return handled;
     }
 }

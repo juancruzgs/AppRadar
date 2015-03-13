634a19e08115d67ca5fa9e1ec21badb9c2a7813d
Paula Baudo <paula.baudo@globant.com>
2015-03-13 15:55:58 -0300

    Some translations & Warnings

--- app/src/main/java/com/mobilemakers/juansoler/appradar/SummaryFragment.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/SummaryFragment.java
@@ -3,13 +3,11 @@ package com.mobilemakers.juansoler.appradar;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
-import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
@@ -21,7 +19,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.afollestad.materialdialogs.AlertDialogWrapper;
 import com.google.android.gms.common.api.GoogleApiClient;

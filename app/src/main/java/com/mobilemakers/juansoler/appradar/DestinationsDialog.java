b6ca1ed4cd28c7e860fec0b7cf9806f1f77c311f
Juan Cruz <juancruz_gs@hotmail.com>
2015-03-16 11:16:58 -0300

    DestinationsDialog refactoring

--- app/src/main/java/com/mobilemakers/juansoler/appradar/DestinationsDialog.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/DestinationsDialog.java
@@ -9,19 +9,16 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class DestinationsDialog extends DialogFragment {
 
+    ArrayList<String> mDestinationsList = new ArrayList<>();
+
     public interface DestinationDialogListener {
         void onFinishDialog(String Destination);
     }
-
-    DestinationsAdapter mAdapter;
-    ArrayList<String> mDestinationsList = new ArrayList<>();
-    DestinationDialogListener mDialogListener;
 
     public DestinationsDialog() {
     }
@@ -30,22 +27,42 @@ public class DestinationsDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.destinations_dialog, container);
-        mDialogListener = (DestinationDialogListener) getFragmentManager().findFragmentById(R.id.container);
+        prepareDialog();
+        prepareListView(rootView);
+        prepareButtonCancel(rootView);
+        return rootView;
+    }
+
+    private void prepareDialog() {
         Dialog dialog = getDialog();
         if (dialog != null){
             dialog.setTitle(getString(R.string.Destinations_dialog_title));
         }
         this.setCancelable(false);
+    }
+
+    private void prepareListView(View rootView) {
         ListView listView = (ListView) rootView.findViewById(R.id.listView_destinations);
-        Collections.addAll(mDestinationsList,getResources().getStringArray(R.array.cities));
-        mAdapter = new DestinationsAdapter(getActivity(), mDestinationsList);
-        listView.setAdapter(mAdapter);
+        setListViewAdapter(listView);
+        setOnItemClickListener(listView);
+    }
+
+    private void setListViewAdapter(ListView listView) {
+        Collections.addAll(mDestinationsList, getResources().getStringArray(R.array.cities));
+        DestinationsAdapter adapter = new DestinationsAdapter(getActivity(), mDestinationsList);
+        listView.setAdapter(adapter);
+    }
+
+    private void setOnItemClickListener(ListView listView) {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 dismissDialog(position);
             }
         });
+    }
+
+    private void prepareButtonCancel(View rootView) {
         Button buttonCancelDialog = (Button)rootView.findViewById(R.id.button_cancel_dialog);
         buttonCancelDialog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,16 +70,15 @@ public class DestinationsDialog extends DialogFragment {
                 dismissDialog(-1);
             }
         });
-        return rootView;
     }
 
     public void dismissDialog(int position){
+        DestinationDialogListener dialogListener = (DestinationDialogListener) getFragmentManager().findFragmentById(R.id.container);
         if (position > -1) {
-            mDialogListener.onFinishDialog(mDestinationsList.get(position));
+            dialogListener.onFinishDialog(mDestinationsList.get(position));
         } else {
-            mDialogListener.onFinishDialog("");
+            dialogListener.onFinishDialog("");
         }
         this.dismiss();
     }
-
 }

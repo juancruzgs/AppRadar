d82034567f3b3e1bbdc0d8e21f9ad761ae44cf65
Juan Cruz <juancruz_gs@hotmail.com>
2015-03-20 09:45:51 -0300

    Notification Preference changes

--- app/src/main/java/com/mobilemakers/juansoler/appradar/NotificationPreference.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/NotificationPreference.java
@@ -22,75 +22,43 @@ public class NotificationPreference {
         return mFirstNotificationDistance;
     }
 
-    public void setFirstNotificationDistance(String mFirstNotificationDistance) {
-        this.mFirstNotificationDistance = mFirstNotificationDistance;
-    }
-
     public String getFirstNotificationSound() {
         return mFirstNotificationSound;
-    }
-
-    public void setFirstNotificationSound(String mFirstNotificationSound) {
-        this.mFirstNotificationSound = mFirstNotificationSound;
     }
 
     public String getFirstNotificationLed() {
         return mFirstNotificationLed;
     }
 
-    public void setFirstNotificationLed(String firstNotificationLed) {
-        mFirstNotificationLed = firstNotificationLed;
-    }
-
     public String getSecondNotificationDistance() {
         return mSecondNotificationDistance;
-    }
-
-    public void setSecondNotificationDistance(String mSecondNotificationDistance) {
-        this.mSecondNotificationDistance = mSecondNotificationDistance;
     }
 
     public String getSecondNotificationSound() {
         return mSecondNotificationSound;
     }
 
-    public void setSecondNotificationSound(String mSecondNotificationSound) {
-        this.mSecondNotificationSound = mSecondNotificationSound;
-    }
-
     public String getSecondNotificationLed() {
         return mSecondNotificationLed;
-    }
-
-    public void setSecondNotificationLed(String secondNotificationLed) {
-        mSecondNotificationLed = secondNotificationLed;
     }
 
     public String getThirdNotificationSound() {
         return mThirdNotificationSound;
     }
 
-    public void setThirdNotificationSound(String mThirdNotificationSound) {
-        this.mThirdNotificationSound = mThirdNotificationSound;
-    }
-
     public String getThirdNotificationLed() {
         return mThirdNotificationLed;
     }
 
-    public void setThirdNotificationLed(String thirdNotificationLed) {
-        mThirdNotificationLed = thirdNotificationLed;
-    }
-
     public void getSharedPreferences (Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        setFirstNotificationDistance(sharedPreferences.getString(Constants.FIRST_NOTIFICATION_DISTANCE, "10"));
-        setFirstNotificationSound(sharedPreferences.getString(Constants.FIRST_NOTIFICATION_SOUND, context.getResources().getStringArray(R.array.sound_values)[0]));
-        setFirstNotificationLed(sharedPreferences.getString(Constants.FIRST_NOTIFICATION_LED, "00FF00"));
-        setSecondNotificationDistance(sharedPreferences.getString(Constants.SECOND_NOTIFICATION_DISTANCE, "4"));
-        setSecondNotificationSound(sharedPreferences.getString(Constants.SECOND_NOTIFICATION_SOUND, context.getResources().getStringArray(R.array.sound_values)[0]));
-        setSecondNotificationLed(sharedPreferences.getString(Constants.SECOND_NOTIFICATION_LED, "FFFF00"));
-        setThirdNotificationSound(sharedPreferences.getString(Constants.THIRD_NOTIFICATION_SOUND, context.getResources().getStringArray(R.array.sound_values)[0]));
-        setThirdNotificationLed(sharedPreferences.getString(Constants.THIRD_NOTIFICATION_LED, "FF0000"));
+        mFirstNotificationDistance = sharedPreferences.getString(Constants.FIRST_NOTIFICATION_DISTANCE, "10");
+        mFirstNotificationSound = sharedPreferences.getString(Constants.FIRST_NOTIFICATION_SOUND, context.getResources().getStringArray(R.array.sound_values)[0]);
+        mFirstNotificationLed = sharedPreferences.getString(Constants.FIRST_NOTIFICATION_LED, "00FF00");
+        mSecondNotificationDistance = sharedPreferences.getString(Constants.SECOND_NOTIFICATION_DISTANCE, "4");
+        mSecondNotificationSound = sharedPreferences.getString(Constants.SECOND_NOTIFICATION_SOUND, context.getResources().getStringArray(R.array.sound_values)[0]);
+        mSecondNotificationLed = sharedPreferences.getString(Constants.SECOND_NOTIFICATION_LED, "FFFF00");
+        mThirdNotificationSound = sharedPreferences.getString(Constants.THIRD_NOTIFICATION_SOUND, context.getResources().getStringArray(R.array.sound_values)[0]);
+        mThirdNotificationLed = sharedPreferences.getString(Constants.THIRD_NOTIFICATION_LED, "FF0000");
     }
 }

752203d0ffef537715933a57cae319ff17cbd371
Ariel Cattáneo <ariel.cattaneo@globant.com>
2015-03-20 09:48:24 -0300

    Intermediate "end screen" commit

--- app/src/main/java/com/mobilemakers/juansoler/appradar/LastRadarFragment.java
+++ app/src/main/java/com/mobilemakers/juansoler/appradar/LastRadarFragment.java
@@ -1,20 +1,28 @@
 package com.mobilemakers.juansoler.appradar;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LastRadarFragment extends Fragment implements MainActivity.OnBackPressedListener {
+
+    private RadarList mRadars;
 
     private Button mButtonEnd;
 
@@ -32,33 +40,62 @@ public class LastRadarFragment extends Fragment implements MainActivity.OnBackPr
                 FragmentManager.POP_BACK_STACK_INCLUSIVE);
     }
 
-    private void prepareEndButton() {
-        mButtonEnd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                endTrip();
-            }
-        });
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState!=null){
+            mRadars = savedInstanceState.getParcelable(Constants.RADARS_LIST);
+        } else {
+            getFragmentArguments();
+        }
     }
 
-    private void wireUpViews(View rootView) {
-        mButtonEnd = (Button) rootView.findViewById(R.id.button_last_radar_end);
+    private void getFragmentArguments() {
+        Bundle bundle = getArguments();
+        if (bundle != null && bundle.containsKey(Constants.RADARS_LIST)) {
+            mRadars = bundle.getParcelable(Constants.RADARS_LIST);
+        }
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // TODO: Add map loading code when it's fixed at SummaryFragment
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_last_radar, container, false);
 
         LinearLayout layoutMain = (LinearLayout)rootView.findViewById(R.id.Layout_Main);
         Transitions.fadeIN(layoutMain, Constants.TRANSIION_DURATION_2K);
-        wireUpViews(rootView);
-        prepareEndButton();
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_fragment_last_radar, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Boolean handled = false;
+
+        switch(id) {
+            case R.id.action_bar:
+                handled = true;
+                break;
+            case R.id.action_show_map:
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                intent.putExtra(Constants.RADARS_LIST, mRadars);
+                startActivity(intent);
+                handled = true;
+                break;
+        }
+
+        if (!handled) {
+            handled = super.onOptionsItemSelected(item);
+        }
+        return handled;
     }
 
 
